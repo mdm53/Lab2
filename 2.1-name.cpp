939362b8365db6298c7ae8b9f8894124867c65db
mdm53 <123202490+mdm53@users.noreply.github.com>
2023-02-10 16:27:16 -0500

    Add files via upload

---
+++ 2.1-name.cpp
@@ -0,0 +1,22 @@
+// This program will write the name, address and telephone
+// number of the programmer.
+
+// Michael Malone
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  
+  cout << "Michael Malone" << endl;
+  cout << "203 Genung St" << endl;
+  cout << "Middletown, NY. 10940" << endl;
+  
+  
+  
+  cout << "845-553-3688" << endl;
+
+  return 0;
+  
+}
---
+++ 2.2-circlearea.cpp
@@ -0,0 +1,24 @@
+// This program will output the circumference and area
+// of the circle with a goven radius
+
+// Mchael Malone
+
+#include <iostream>
+using namespace std;
+
+const double PI = 3.14;
+const double RADIUS = 5.4;
+
+int main()
+
+{
+  _area                            // definition of area of a circle
+  
+  float circumference;             // definition of circumference
+
+  circumference = 2 * PI * RADIUS; // computes circumference
+
+  area = _;                        // computes area
+
+  return 
+}
